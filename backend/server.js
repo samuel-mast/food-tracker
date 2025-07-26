9d7a8b95351405066fa09c350d50f7054195e8cd
Ben-Mast <benjaminmst@gmail.com>
2025-07-26 13:21:05 -0500

    actually changed mongodb connect statement

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {useNewURIParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI)
 .then(()=> console.log("MongoDB has landed on ur chin"))
 .catch(err => console.error(err));
 
